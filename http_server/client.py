b59a8870b4c14be0d7ed7b8f466e4020b4245661
huangyizhao <huangyizhao@b.360.cn>
2018-09-29 18:31:28 +0800

    Add client

---
+++ http_server/client.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+import os
+import socket
+import argparse
+
+SERVER_ADDRESS = "10.91.3.37", 8888
+REQUEST = """
+GET /hello HTTP/1.1
+Host: 10.91.3.37:8888
+"""
+
+
+def send_requests(max_clients, max_conns):
+    print max_clients, max_conns
+    socks = []
+    for client_num in range(max_clients):
+        pid = os.fork()
+        if pid == 0:
+            for connection_num in range(max_conns):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect(SERVER_ADDRESS)
+                sock.sendall(REQUEST)
+                socks.append(sock)
+                print connection_num
+                os._exit(0)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="test client",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--max-conns",
+        type=int,
+        default=1024,
+        help="Maximum number of connections per client"
+    )
+    parser.add_argument(
+        "--max-clients",
+        type=int,
+        default=1,
+        help="Maximum number of clients"
+    )
+    args = parser.parse_args()
+    send_requests(args.max_clients, args.max_conns)
