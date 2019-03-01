05058a4ac93783bf07a61261c637737762e381a5
huangyizhao <huangyizhao@360.net>
2019-03-01 16:33:14 +0800

    Update decorators

--- intermediate_python/decorators.py
+++ intermediate_python/decorators.py
@@ -25,3 +25,34 @@ a_function_requiring_decoration("abc", b="def")
 
 # print wrap_the_function if there is not @wraps
 print(a_function_requiring_decoration.__name__)
+
+
+
+class Logit(object):
+    def __init__(self, logfile='out.log'):
+        self.logfile = logfile
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            log_string = func.__name__ + " was called"
+            print(log_string)
+
+            with open(self.logfile, 'a') as opened_file:
+                opened_file.write(log_string + "\n")
+
+            self.notify()
+            return func(*args, **kwargs)
+
+        return wrapped_function
+
+    def notify(self):
+        pass
+
+
+@Logit()
+def my_func():
+    print("in my func")
+
+
+my_func()
