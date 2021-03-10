1bf5302139c0acb08d40bd03f0342d4bd9879ad7
eazow <eazow@163.com>
2021-03-10 15:55:51 +0800

    Add selection sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,13 +1,8 @@
-# -*- coding:utf-8 -*-
-
-
 def quick_sort_recursion(nums, left, right):
     if left >= right:
         return
 
-    i = left
-    j = right
-    base_num = nums[left]
+    i, j, base_num = left, right, nums[left]
 
     while i < j:
         # 从右向左寻找比base_num小的数
@@ -35,5 +30,5 @@ def quick_sort(nums):
 
 
 if __name__ == "__main__":
-    the_nums = [10, 30, 5, 1, 100, 20, 6, 50]
-    assert quick_sort(the_nums) == [1, 5, 6, 10, 20, 30, 50, 100]
+    nums = [10, 30, 5, 1, 100, 20, 6, 50]
+    assert quick_sort(nums) == [1, 5, 6, 10, 20, 30, 50, 100]
