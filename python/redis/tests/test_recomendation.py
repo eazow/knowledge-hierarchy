ff1f21301381f0cb760c44d1a8b7f8cfd64f08f6
eazow <eazow@163.com>
2021-06-21 20:22:44 +0800

    Add recommendation

---
+++ python/redis/tests/test_recomendation.py
@@ -0,0 +1,40 @@
+from redis import Redis
+
+from recommendation import Recommendation
+
+
+class TestRecommendation:
+    def setup_class(self):
+        self.client = Redis(decode_responses=True)
+
+    def test_recommendation(self):
+        recommendation = Recommendation(self.client)
+
+        recommendation.forward_to("book1", "book2")
+        recommendation.forward_to("book1", "book2")
+        recommendation.forward_to("book1", "book2")
+        recommendation.forward_to("book1", "book3")
+        recommendation.forward_to("book1", "book3")
+        recommendation.forward_to("book1", "book4")
+        recommendation.forward_to("book1", "book5")
+        recommendation.forward_to("book1", "book6")
+        recommendation.forward_to("book1", "book6")
+
+        assert recommendation.get_recommendations("book1", 1, 5) == [
+            "book2",
+            "book6",
+            "book3",
+            "book5",
+            "book4",
+        ]
+
+        assert recommendation.get_recommendations("book1", 1, 5, with_time=True) == [
+            ("book2", 3),
+            ("book6", 2),
+            ("book3", 2),
+            ("book5", 1),
+            ("book4", 1),
+        ]
+
+    def teardown_class(self):
+        self.client.close()
