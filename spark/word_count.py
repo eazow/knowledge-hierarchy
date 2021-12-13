d3e7e9a412ebb1e4b5c15768791d393908b92f9e
eazow <eazow@163.com>
2021-12-13 17:36:37 +0800

    Update word count

--- spark/word_count.py
+++ spark/word_count.py
@@ -1,11 +1,12 @@
 from pyspark import SparkContext
 
-textFile = SparkContext.getOrCreate().textFile("./data/spark.txt")
+text = SparkContext.getOrCreate().textFile("./data/spark.txt")
 top5_words = (
-    textFile.flatMap(lambda line: line.split(" "))
+    text.flatMap(lambda line: line.split(" "))
     .filter(lambda word: word != "")
     .map(lambda word: (word, 1))
     .reduceByKey(lambda x, y: x + y)
     .sortBy(lambda x: x[1], False)
     .take(5)
 )
+print(top5_words)
