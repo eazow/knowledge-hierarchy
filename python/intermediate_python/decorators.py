9732c9a92ecb87edac11931d2738b8c51543875f
hhmhywzr <eazow@163.com>
2022-01-11 23:57:18 +0800

    Update decorator

--- python/intermediate_python/decorators.py
+++ python/intermediate_python/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 
 
-def a_new_docorator(a_func):
+def a_new_decorator(a_func):
     @wraps(a_func)
     def wrap_the_function(*args, **kwargs):
         print("before a_func()")
@@ -11,7 +11,7 @@ def a_new_docorator(a_func):
     return wrap_the_function
 
 
-@a_new_docorator
+@a_new_decorator
 def a_function_requiring_decoration(a, b):
     print("function which needs some decoration")
     print("arguments: %s, %s" % (a, b))
@@ -20,7 +20,7 @@ def a_function_requiring_decoration(a, b):
 a_function_requiring_decoration("abc", b="def")
 
 # the @a_new_decorator is just a short way of saying
-# a_function_requiring_decoration = a_new_docorator(a_function_requiring_decoration)
+# a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
 
 # print wrap_the_function if there is not @wraps
 print(a_function_requiring_decoration.__name__)
