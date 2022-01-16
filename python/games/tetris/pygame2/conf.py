37e4455459daaa89f23ead1d5d6dca696739af01
hhmhywzr <eazow@163.com>
2022-01-16 23:57:30 +0800

    Refactoring

--- python/games/tetris/pygame2/conf.py
+++ python/games/tetris/pygame2/conf.py
@@ -3,3 +3,12 @@ window_height = 700
 play_width = 300  # meaning 300 // 10 = 30 width per block
 play_height = 600  # meaning 600 // 20 = 20 height per blo ck
 block_size = 30
+shape_colors = [
+    (0, 255, 0),
+    (255, 0, 0),
+    (0, 255, 255),
+    (255, 255, 0),
+    (255, 165, 0),
+    (0, 0, 255),
+    (128, 0, 128),
+]
