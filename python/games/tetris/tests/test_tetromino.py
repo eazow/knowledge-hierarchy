8be57619cba2e6d4247017c1a2a8106e03da267e
eazow <eazow@163.com>
2022-02-17 19:24:55 +0800

    Update tetris

--- python/games/tetris/tests/test_tetromino.py
+++ python/games/tetris/tests/test_tetromino.py
@@ -4,9 +4,8 @@ from tetromino import Block, BlockS
 def test_block_s():
     t = BlockS(5, 0)
     assert [(5, -2), (6, -2), (4, -1), (5, -1)] == t.coordinates
-
-    t.rotate()
-    assert [(5, -3), (5, -2), (6, -2), (6, -1)] == t.coordinates
+    assert [(5, -3), (5, -2), (6, -2), (6, -1)] == t.rotate().coordinates
+    assert [(5, -2), (6, -2), (4, -1), (5, -1)] == t.rotate().coordinates
 
 
 def test_create_block():
