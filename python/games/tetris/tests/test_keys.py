ae6e797b1d20fc579a67711748ae91be87b02c5f
eazow <eazow@163.com>
2022-02-21 11:47:29 +0800

    Update test cases

--- python/games/tetris/tests/test_keys.py
+++ python/games/tetris/tests/test_keys.py
@@ -1,7 +1,7 @@
 from colors import Color
 from conf import rows
 from grid import Grid
-from handler import handle_key_space, handle_key_left
+from handler import handle_key_space, handle_key_left, handle_key_up
 from tetromino import BlockO
 
 
@@ -37,3 +37,19 @@ def test_key_left():
     assert grid.colors_by_row_col[0][4] == BlockO.color
     assert grid.colors_by_row_col[1][3] == BlockO.color
     assert grid.colors_by_row_col[1][4] == BlockO.color
+
+
+def test_key_up():
+    grid = Grid()
+    grid.current_block = BlockO(5, 0)
+
+    grid.fall_block()
+    grid.fall_block()
+
+    handle_key_up(grid)
+    grid.update_colors()
+
+    assert grid.colors_by_row_col[0][4] == BlockO.color
+    assert grid.colors_by_row_col[0][5] == BlockO.color
+    assert grid.colors_by_row_col[1][4] == BlockO.color
+    assert grid.colors_by_row_col[1][5] == BlockO.color
