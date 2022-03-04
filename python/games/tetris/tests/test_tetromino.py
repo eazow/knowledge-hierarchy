e6da31f6a83346941a4b45385cc77f65afdd16c8
eazow <eazow@163.com>
2022-03-04 16:56:36 +0800

    Update tetris

--- python/games/tetris/tests/test_tetromino.py
+++ python/games/tetris/tests/test_tetromino.py
@@ -2,17 +2,17 @@ from tetromino import Block, BlockS, BlockZ
 
 
 def test_block_s():
-    t = BlockS(5, 0)
-    assert [(5, -2), (6, -2), (4, -1), (5, -1)] == t.coordinates
-    assert [(5, -3), (5, -2), (6, -2), (6, -1)] == t.rotate().coordinates
-    assert [(5, -2), (6, -2), (4, -1), (5, -1)] == t.rotate().coordinates
+    t = BlockS(3, -4)
+    assert [(5, -3), (6, -3), (4, -2), (5, -2)] == t.coordinates
+    assert [(4, -4), (4, -3), (5, -3), (5, -2)] == t.rotate().coordinates
+    assert [(5, -3), (6, -3), (4, -2), (5, -2)] == t.rotate().coordinates
 
 
 def test_block_z():
-    t = BlockZ(5, 0)
-    assert [(4, -2), (5, -2), (5, -1), (6, -1)] == t.coordinates
-    assert [(5, -3), (4, -2), (5, -2), (4, -1)] == t.rotate().coordinates
-    assert [(4, -2), (5, -2), (5, -1), (6, -1)] == t.rotate().coordinates
+    t = BlockZ(3, -4)
+    assert [(4, -3), (5, -3), (5, -2), (6, -2)] == t.coordinates
+    assert [(5, -4), (4, -3), (5, -3), (4, -2)] == t.rotate().coordinates
+    assert [(4, -3), (5, -3), (5, -2), (6, -2)] == t.rotate().coordinates
 
 
 def test_create_block():
--- python/games/tetris/tetromino.py
+++ python/games/tetris/tetromino.py
@@ -1,17 +1,50 @@
 """
 10 x 20 square grid
 shapes: S, Z, I, O, J, L, T
-represented in order by 0 - 6
 """
 import random
 from abc import ABC
-from enum import Enum
 
 from colors import Color
 
 
-class BlockShapes(Enum):
-    S = [
+class Block(ABC):
+    shapes = None
+    color = None
+    rotation = 0
+
+    def __init__(self, col, row):
+        self.col = col
+        self.row = row
+
+    @property
+    def coordinates(self):
+        shape = self.shapes[self.rotation]
+
+        return [
+            (self.col + j, self.row + i)
+            for i, row in enumerate(shape)
+            for j, col in enumerate(row)
+            if col != "."
+        ]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.shapes)
+        return self
+
+    def fall(self):
+        self.row += 1
+
+    def rise(self):
+        self.row -= 1
+
+    @classmethod
+    def create(cls, row, col):
+        return random.choice(cls.__subclasses__())(row, col)
+
+
+class BlockS(Block):
+    shapes = [
         [
             "....",
             "..00",
@@ -25,7 +58,11 @@ class BlockShapes(Enum):
             "....",
         ],
     ]
-    Z = [
+    color = Color.YELLOW
+
+
+class BlockZ(Block):
+    shapes = [
         [
             "....",
             ".00.",
@@ -39,7 +76,11 @@ class BlockShapes(Enum):
             "....",
         ],
     ]
-    I = [
+    color = Color.TEAL
+
+
+class BlockI(Block):
+    shapes = [
         [
             ".0..",
             ".0..",
@@ -53,7 +94,11 @@ class BlockShapes(Enum):
             "....",
         ],
     ]
-    O = [
+    color = Color.BLUE
+
+
+class BlockO(Block):
+    shapes = [
         [
             "....",
             ".00.",
@@ -61,7 +106,11 @@ class BlockShapes(Enum):
             "....",
         ]
     ]
-    J = [
+    color = Color.ORANGE
+
+
+class BlockJ(Block):
+    shapes = [
         [
             "....",
             ".0..",
@@ -87,7 +136,11 @@ class BlockShapes(Enum):
             "....",
         ],
     ]
-    L = [
+    color = Color.GREEN
+
+
+class BlockL(Block):
+    shapes = [
         [
             "....",
             "..0.",
@@ -113,123 +166,34 @@ class BlockShapes(Enum):
             "....",
         ],
     ]
-    T = [
-        [
-            "....",
-            ".0..",
-            "000.",
-            "....",
-        ],
-        [
-            ".0..",
-            ".00.",
-            ".0..",
-            "....",
-        ],
-        [
-            "....",
-            "000.",
-            ".0..",
-            "....",
-        ],
-        [
-            "..0.",
-            ".00.",
-            "..0.",
-            "....",
-        ],
-    ]
-
-
-class Block(ABC):
-    shapes = None
-    color = None
-    rotation = 0
-
-    def __init__(self, col, row):
-        self.col = col
-        self.row = row
-
-    @property
-    def coordinates(self):
-        shape = self.shapes[self.rotation]
-
-        return [
-            (self.col + j - 2, self.row + i - 4)
-            for i, row in enumerate(shape)
-            for j, col in enumerate(row)
-            if col != "."
-        ]
-
-    def rotate(self):
-        self.rotation = (self.rotation + 1) % len(self.shapes)
-        return self
-
-    def fall(self):
-        self.row += 1
-
-    def rise(self):
-        self.row -= 1
-
-    @classmethod
-    def create(cls, row, col):
-        return random.choice(cls.__subclasses__())(row, col)
-
-
-class BlockS(Block):
-    shapes = [
-        [".....", ".....", "..00.", ".00..", "....."],
-        [".....", "..0..", "..00.", "...0.", "....."],
-    ]
-    color = Color.YELLOW
-
-
-class BlockZ(Block):
-    shapes = [
-        [".....", ".....", ".00..", "..00.", "....."],
-        [".....", "..0..", ".00..", ".0...", "....."],
-    ]
-    color = Color.TEAL
-
-
-class BlockI(Block):
-    shapes = [
-        ["..0..", "..0..", "..0..", "..0..", "....."],
-        [".....", "0000.", ".....", ".....", "....."],
-    ]
-    color = Color.BLUE
-
-
-class BlockO(Block):
-    shapes = [[".....", ".....", ".00..", ".00..", "....."]]
-    color = Color.ORANGE
-
-
-class BlockJ(Block):
-    shapes = [
-        [".....", ".0...", ".000.", ".....", "....."],
-        [".....", "..00.", "..0..", "..0..", "....."],
-        [".....", ".....", ".000.", "...0.", "....."],
-        [".....", "..0..", "..0..", ".00..", "....."],
-    ]
-    color = Color.GREEN
-
-
-class BlockL(Block):
-    shapes = [
-        [".....", "...0.", ".000.", ".....", "....."],
-        [".....", "..0..", "..0..", "..00.", "....."],
-        [".....", ".....", ".000.", ".0...", "....."],
-        [".....", ".00..", "..0..", "..0..", "....."],
-    ]
     color = Color.RED
 
 
 class BlockT(Block):
     shapes = [
-        [".....", "..0..", ".000.", ".....", "....."],
-        [".....", "..0..", "..00.", "..0..", "....."],
-        [".....", ".....", ".000.", "..0..", "....."],
-        [".....", "..0..", ".00..", "..0..", "....."],
+        [
+            "....",
+            "..0.",
+            ".000",
+            "....",
+        ],
+        [
+            ".0..",
+            ".00.",
+            ".0..",
+            "....",
+        ],
+        [
+            "....",
+            ".000",
+            "..0.",
+            "....",
+        ],
+        [
+            "..0.",
+            ".00.",
+            "..0.",
+            "....",
+        ],
     ]
     color = Color.PURPLE
