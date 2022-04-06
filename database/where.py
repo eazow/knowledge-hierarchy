e0e31334bca5a8bb636aeecb47988910e136b985
eazow <eazow@163.com>
2022-04-06 16:22:51 +0800

    Update where

--- database/where.py
+++ database/where.py
@@ -2,15 +2,15 @@ from utils import _slots
 
 
 class _Where:
-    "_Where(mode, condition) -> _Where"
+    """_Where(mode, condition) -> _Where"""
 
     __slots__ = _slots("call rows")
 
     def __init__(self, mode, condition):
-        "Initializes _Where support object for simple selections."
+        """Initializes _Where support object for simple selections."""
         self.__call = {"and": all, "or": any}[mode]
         self.__rows = condition
 
     def __call__(self, row):
-        "Runs test on given row and validates against condition."
+        """Runs test on given row and validates against condition."""
         return self.__call(row[k] == v for k, v in self.__rows.items())
