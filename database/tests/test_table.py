8047aea45a3504a1316ddf528e59b8fc14abb35b
eazow <eazow@163.com>
2022-04-08 20:55:02 +0800

    Update test cases

--- database/tests/test_table.py
+++ database/tests/test_table.py
@@ -2,13 +2,16 @@ from row import ROW
 
 
 def test_create(persons):
-    assert persons == """\
+    assert (
+        persons
+        == """\
 P_ID LASTNAME  FIRSTNAME ADDRESS      CITY     
 ---- --------- --------- ------------ ---------
    1 Hansen    Ola       Timoteivn 10 Sandnes  
    2 Svendson  Tove      Borgvn 23    Sandnes  
    3 Pettersen Kari      Storgt 20    Stavanger\
 """
+    )
 
 
 def test_repr(persons):  # persons.print()
@@ -25,30 +28,63 @@ def test_repr(persons):  # persons.print()
 
 
 def test_select(persons):
-    assert persons.select("LastName", "FirstName") == """\
+    assert (
+        persons.select("LastName", "FirstName")
+        == """\
 LASTNAME  FIRSTNAME
 --------- ---------
 Hansen    Ola      
 Svendson  Tove     
 Pettersen Kari     \
 """
+    )
 
-    assert persons.select() == """\
+    assert (
+        persons.select()
+        == """\
 P_ID LASTNAME  FIRSTNAME ADDRESS      CITY     
 ---- --------- --------- ------------ ---------
    1 Hansen    Ola       Timoteivn 10 Sandnes  
    2 Svendson  Tove      Borgvn 23    Sandnes  
    3 Pettersen Kari      Storgt 20    Stavanger\
 """
+    )
 
 
 def test_distinct(persons):
-    # Test the distinct statement.
-    persons.select("City").distinct().print()
-    # Test the where clause.
-    persons.where(ROW.City == "Sandnes").print()
-    # Test the and operator.
-    persons.where((ROW.FirstName == "Tove") & (ROW.LastName == "Svendson")).print()
+    assert (
+        persons.select("City").distinct()
+        == """\
+CITY     
+---------
+Sandnes  
+Stavanger\
+"""
+    )
+
+
+def test_where(persons):
+    assert (
+        persons.where(ROW.City == "Sandnes")
+        == """\
+P_ID LASTNAME FIRSTNAME ADDRESS      CITY   
+---- -------- --------- ------------ -------
+   1 Hansen   Ola       Timoteivn 10 Sandnes
+   2 Svendson Tove      Borgvn 23    Sandnes\
+"""
+    )
+
+
+def test_and(persons):
+    assert (
+        persons.where((ROW.FirstName == "Tove") & (ROW.LastName == "Svendson"))
+        == """\
+P_ID LASTNAME FIRSTNAME ADDRESS      CITY
+---- -------- --------- ------------ -------
+   2 Svendson Tove      Borgvn 23    Sandnes\
+"""
+    )
+
     # Test the or operator.
     persons.where((ROW.FirstName == "Tove") | (ROW.FirstName == "Ola")).print()
     # Test both and & or operators.
@@ -56,16 +92,22 @@ def test_distinct(persons):
         (ROW.LastName == "Svendson")
         & ((ROW.FirstName == "Tove") | (ROW.FirstName == "Ola"))
     ).print()
-    # Test order by statement.
+
+
+def test_order_by(persons):
     persons.insert(4, "Nilsen", "Tom", "Vingvn 23", "Stavanger")
     persons.order_by("LastName").table().print()
     persons.order_by("LastName", True).table().print()
-    # Test insert statement.
+
+
+def test_insert(persons):
     persons.insert(5, "Nilsen", "Johan", "Bakken 2", "Stavanger")
     persons.print()
     persons.insert(P_Id=6, LastName="Tjessem", FirstName="Jakob")
     persons.print()
-    # Test update statement.
+
+
+def test_update(persons):
     persons.where((ROW.LastName == "Tjessem") & (ROW.FirstName == "Jakob")).update(
         Address="Nissestien 67", City="Sandnes"
     )
@@ -73,8 +115,9 @@ def test_distinct(persons):
     copy = persons.order_by("P_Id").table()
     copy.update(Address="Nissestien 67", City="Sandnes")
     copy.print()
-    # Test delete statement.
+
+
+def test_delete(persons):
     copy = persons.order_by("P_Id").table()
     copy.delete((ROW.LastName == "Tjessem") & (ROW.FirstName == "Jakob")).print()
     copy.truncate().print()
-    return persons
