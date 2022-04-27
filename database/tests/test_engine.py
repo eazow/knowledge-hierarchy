f5cfa8c8450f33eac67550b3d8a812e100fe91e6
hhmhywzr <eazow@163.com>
2022-04-27 23:47:48 +0800

    Add test_join.py

--- database/tests/test_engine.py
+++ database/tests/test_engine.py
@@ -1,9 +1,9 @@
 import sys
 
-from engine import Database, NOW
+from engine import Database
 from row import ROW
-from table import Table, inner_join, left_join, right_join, full_join, union
-from utils import MID, date, datetime, FORMAT
+from table import Table, inner_join, union
+from utils import date, datetime
 
 """
 def test(persons):
@@ -32,57 +32,6 @@ def test(persons):
     # Allow for interaction at the end of the test.
     globals().update(locals())
 """
-
-
-def test_inner_join(persons, orders):
-    assert (
-        inner_join(ROW.Persons.P_Id == ROW.Orders.P_Id, Persons=persons, Orders=orders)
-        .select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo")
-        .order_by("Persons.LastName")
-        .table()
-        == """\
-PERSONS.LASTNAME PERSONS.FIRSTNAME ORDERS.ORDERNO
----------------- ----------------- --------------
-Hansen           Ola                        22456
-Hansen           Ola                        24562
-Pettersen        Kari                       77895
-Pettersen        Kari                       44678\
-"""
-    )
-
-
-def test_all_joins(persons, orders):
-    """Tests the four different types of joins in SQL."""
-
-    # Test inner join with alias.
-    inner_join(ROW.p.P_Id == ROW.po.P_Id, p=persons, po=orders).select(
-        "po.OrderNo", "p.LastName", "p.FirstName"
-    ).where((ROW.p.LastName == "Hansen") & (ROW.p.FirstName == "Ola")).print()
-    # Test left join with and without alias.
-    left_join(
-        (persons, "Persons"), (orders, "Orders"), ROW.Persons.P_Id == ROW.Orders.P_Id
-    ).select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo").order_by(
-        "Persons.LastName"
-    ).table().print()
-    left_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id).select(
-        "p.LastName", "p.FirstName", "o.OrderNo"
-    ).order_by("p.LastName").table().print()
-    # Test right join with and without alias.
-    right_join(
-        (persons, "Persons"), (orders, "Orders"), ROW.Persons.P_Id == ROW.Orders.P_Id
-    ).select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo").order_by(
-        "Persons.LastName"
-    ).table().print()
-    right_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id).select(
-        "p.LastName", "p.FirstName", "o.OrderNo"
-    ).order_by("p.LastName").table().print()
-    # Test full join with and without alias.
-    full_join(
-        ROW.Persons.P_Id == ROW.Orders.P_Id, Persons=persons, Orders=orders
-    ).order_by("Persons.LastName").table().print()
-    full_join(ROW.p.P_Id == ROW.o.P_Id, p=persons, o=orders).select(
-        "p.LastName", "p.FirstName", "o.OrderNo"
-    ).order_by("p.LastName").table().print()
 
 
 def test_table_addition(persons, orders):
---
+++ database/tests/test_join.py
@@ -0,0 +1,49 @@
+def test_inner_join(persons, orders):
+    assert (
+        inner_join(ROW.Persons.P_Id == ROW.Orders.P_Id, Persons=persons, Orders=orders)
+        .select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo")
+        .order_by("Persons.LastName")
+        .table()
+        == """\
+PERSONS.LASTNAME PERSONS.FIRSTNAME ORDERS.ORDERNO
+---------------- ----------------- --------------
+Hansen           Ola                        22456
+Hansen           Ola                        24562
+Pettersen        Kari                       77895
+Pettersen        Kari                       44678\
+"""
+    )
+
+
+def test_all_joins(persons, orders):
+    """Tests the four different types of joins in SQL."""
+
+    # Test inner join with alias.
+    inner_join(ROW.p.P_Id == ROW.po.P_Id, p=persons, po=orders).select(
+        "po.OrderNo", "p.LastName", "p.FirstName"
+    ).where((ROW.p.LastName == "Hansen") & (ROW.p.FirstName == "Ola")).print()
+    # Test left join with and without alias.
+    left_join(
+        (persons, "Persons"), (orders, "Orders"), ROW.Persons.P_Id == ROW.Orders.P_Id
+    ).select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo").order_by(
+        "Persons.LastName"
+    ).table().print()
+    left_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id).select(
+        "p.LastName", "p.FirstName", "o.OrderNo"
+    ).order_by("p.LastName").table().print()
+    # Test right join with and without alias.
+    right_join(
+        (persons, "Persons"), (orders, "Orders"), ROW.Persons.P_Id == ROW.Orders.P_Id
+    ).select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo").order_by(
+        "Persons.LastName"
+    ).table().print()
+    right_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id).select(
+        "p.LastName", "p.FirstName", "o.OrderNo"
+    ).order_by("p.LastName").table().print()
+    # Test full join with and without alias.
+    full_join(
+        ROW.Persons.P_Id == ROW.Orders.P_Id, Persons=persons, Orders=orders
+    ).order_by("Persons.LastName").table().print()
+    full_join(ROW.p.P_Id == ROW.o.P_Id, p=persons, o=orders).select(
+        "p.LastName", "p.FirstName", "o.OrderNo"
+    ).order_by("p.LastName").table().print()
