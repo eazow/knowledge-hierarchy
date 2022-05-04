8ac6100934a87b56909cdd209f6893015a255af4
eazow <eazow@163.com>
2022-05-04 23:59:28 +0800

    Update tests

--- database/tests/test_join.py
+++ database/tests/test_join.py
@@ -18,25 +18,54 @@ Pettersen        Kari                       44678\
 """
     )
 
-    assert inner_join(ROW.p.P_Id == ROW.po.P_Id, p=persons, po=orders).select(
-        "po.OrderNo", "p.LastName", "p.FirstName"
-    ).where((ROW.p.LastName == "Hansen") & (ROW.p.FirstName == "Ola")) == """\
+    assert (
+        inner_join(ROW.p.P_Id == ROW.po.P_Id, p=persons, po=orders)
+        .select("po.OrderNo", "p.LastName", "p.FirstName")
+        .where((ROW.p.LastName == "Hansen") & (ROW.p.FirstName == "Ola"))
+        == """\
 PERSONS.LASTNAME PERSONS.FIRSTNAME ORDERS.ORDERNO
 ---------------- ----------------- --------------
 Hansen           Ola                        22456
 Hansen           Ola                        24562\
 """
+    )
 
 
 def test_left_join(persons, orders):
-    left_join(
-        (persons, "Persons"), (orders, "Orders"), ROW.Persons.P_Id == ROW.Orders.P_Id
-    ).select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo").order_by(
-        "Persons.LastName"
-    ).table().print()
-    left_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id).select(
-        "p.LastName", "p.FirstName", "o.OrderNo"
-    ).order_by("p.LastName").table().print()
+    assert (
+        left_join(
+            (persons, "Persons"),
+            (orders, "Orders"),
+            ROW.Persons.P_Id == ROW.Orders.P_Id,
+        )
+        .select("Persons.LastName", "Persons.FirstName", "Orders.OrderNo")
+        .order_by("Persons.LastName")
+        .table()
+        == """\
+PERSONS.LASTNAME PERSONS.FIRSTNAME ORDERS.ORDERNO
+---------------- ----------------- --------------
+Hansen           Ola                        22456
+Hansen           Ola                        24562
+Pettersen        Kari                       77895
+Pettersen        Kari                       44678
+Svendson         Tove                           0\
+"""
+    )
+    assert (
+        left_join((persons, "p"), (orders, "o"), ROW.p.P_Id == ROW.o.P_Id)
+        .select("p.LastName", "p.FirstName", "o.OrderNo")
+        .order_by("p.LastName")
+        .table()
+        == """\
+P.LASTNAME P.FIRSTNAME O.ORDERNO
+---------- ----------- ---------
+Hansen     Ola             22456
+Hansen     Ola             24562
+Pettersen  Kari            77895
+Pettersen  Kari            44678
+Svendson   Tove                0\
+"""
+    )
 
 
 def test_right_join(persons, orders):
