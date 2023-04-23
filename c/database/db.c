6985ef97e8eaa383b02da7a1820d1d670b9297e4
Eazow <eazow@163.com>
2023-04-23 23:47:25 +0800

    Update database

--- c/database/db.c
+++ c/database/db.c
@@ -57,18 +57,26 @@ int main(int argc, char *argv[])
             close_input_buffer(input_buffer);
             exit(EXIT_SUCCESS);
         }
-        else
-        {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-            if (input_buffer->buffer[0] == '.') {
-                switch (do_meta_command(input_buffer)) {
-                    case (META_COMMAND_SUCCESS):
-                        continue;
-                    case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                        printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+                case (META_COMMAND_SUCCESS):
                     continue;
-                }
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
             }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+        continue;
+        
+        execute_statement(&statement);
+        printf("Executed.\n");
+    
     }
 }
