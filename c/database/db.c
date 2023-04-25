2b3700ad53ce9957d92e45942b76e0d9fb552a7e
Eazow <eazow@163.com>
2023-04-25 23:47:25 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -65,6 +65,16 @@ typedef struct
     Row row_to_insert; // only used by insert statement
 } Statement;
 
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+typedef struct
+{
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
+
 void print_prompt() { printf("db > "); }
 
 void read_input(InputBuffer *input_buffer)
