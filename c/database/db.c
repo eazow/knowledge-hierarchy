10f9ce80106936ea2175e37e2f3fc3ac4f0af806
Eazow <eazow@163.com>
2023-04-26 23:42:34 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -140,11 +140,57 @@ void execute_statement(Statement *statement)
     {
     case (STATEMENT_INSERT):
         printf("This is where we would do an insert.\n");
-        break;
+        return execute_insert(statement, table);
     case (STATEMENT_SELECT):
         printf("This is where we would do a select.\n");
-        break;
+        return execute_select(statement, table);
     }
+}
+
+ExecuteResult execute_insert(Statement *statement, Table *table)
+{
+    if (table->num_rows >= TABLE_MAX_ROWS)
+    {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement, Table *table)
+{
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; i++)
+    {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+Table *new_table()
+{
+    Table *table = (Table *)malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    {
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table *table)
+{
+    for (int i = 0; table->pages[i]; i++)
+    {
+        free(table->pages[i]);
+    }
+    free(table);
 }
 
 /**
