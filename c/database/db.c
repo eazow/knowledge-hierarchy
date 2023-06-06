74f3780097369b1401a1c79c69541c8d7043ffab
Eazow <eazow@163.com>
2023-06-07 01:37:56 +0800

    Update db.c

--- c/database/db.c
+++ c/database/db.c
@@ -182,10 +182,11 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
-MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
 {
     if (strcmp(input_buffer->buffer, ".exit") == 0)
     {
+        db_close(table);
         exit(EXIT_SUCCESS);
     }
     else
@@ -332,6 +333,32 @@ void free_table(Table *table)
     free(table);
 }
 
+void pager_flush(Pager *pager, uint32_t page_num, uint32_t size)
+{
+    if (pager->pages[page_num] == NULL)
+    {
+        printf("Tried to flush null page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if (offset == -1)
+    {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written =
+        write(pager->file_descriptor, pager->pages[page_num], size);
+
+    if (bytes_written == -1)
+    {
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
 void db_close(Table *table)
 {
     Pager *pager = table->pager;
@@ -389,18 +416,20 @@ total    291
 
 int main(int argc, char *argv[])
 {
-    Table *table = new_table();
+    if (argc < 2)
+    {
+        printf("Must supply a database filename.\n");
+        exit(EXIT_FAILURE);
+    }
+    char *filename = argv[1];
+    Table *table = db_open(filename);
+
     InputBuffer *input_buffer = new_input_buffer();
     while (true)
     {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
-        {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
         if (input_buffer->buffer[0] == '.')
         {
             switch (do_meta_command(input_buffer))
