9d26dfe3f5a8c4b952fd07983135ec3fe8b63a92
Eazow <eazow@163.com>
2023-06-08 01:01:40 +0800

    Update db.c

--- c/database/db.c
+++ c/database/db.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define TABLE_MAX_PAGES 100
 #define COLUMN_USERNAME_SIZE 32
@@ -182,6 +183,79 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+void pager_flush(Pager *pager, uint32_t page_num, uint32_t size)
+{
+    if (pager->pages[page_num] == NULL)
+    {
+        printf("Tried to flush null page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+    if (offset == -1)
+    {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written =
+        write(pager->file_descriptor, pager->pages[page_num], size);
+
+    if (bytes_written == -1)
+    {
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void db_close(Table *table)
+{
+    Pager *pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++)
+    {
+        if (pager->pages[i] == NULL)
+        {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0)
+    {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != NULL)
+        {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1)
+    {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+    {
+        void *page = pager->pages[i];
+        if (page)
+        {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+    free(pager);
+    free(table);
+}
+
 MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
 {
     if (strcmp(input_buffer->buffer, ".exit") == 0)
@@ -330,79 +404,6 @@ void free_table(Table *table)
     {
         free(table->pages[i]);
     }
-    free(table);
-}
-
-void pager_flush(Pager *pager, uint32_t page_num, uint32_t size)
-{
-    if (pager->pages[page_num] == NULL)
-    {
-        printf("Tried to flush null page\n");
-        exit(EXIT_FAILURE);
-    }
-
-    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
-
-    if (offset == -1)
-    {
-        printf("Error seeking: %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
-
-    ssize_t bytes_written =
-        write(pager->file_descriptor, pager->pages[page_num], size);
-
-    if (bytes_written == -1)
-    {
-        printf("Error writing: %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
-}
-
-void db_close(Table *table)
-{
-    Pager *pager = table->pager;
-    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
-
-    for (uint32_t i = 0; i < num_full_pages; i++)
-    {
-        if (pager->pages[i] == NULL)
-        {
-            continue;
-        }
-        pager_flush(pager, i, PAGE_SIZE);
-        free(pager->pages[i]);
-        pager->pages[i] = NULL;
-    }
-
-    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
-    if (num_additional_rows > 0)
-    {
-        uint32_t page_num = num_full_pages;
-        if (pager->pages[page_num] != NULL)
-        {
-            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
-            free(pager->pages[page_num]);
-            pager->pages[page_num] = NULL;
-        }
-    }
-
-    int result = close(pager->file_descriptor);
-    if (result == -1)
-    {
-        printf("Error closing db file.\n");
-        exit(EXIT_FAILURE);
-    }
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i)
-    {
-        void *page = pager->pages[i];
-        if (page)
-        {
-            free(page);
-            pager->pages[i] = NULL;
-        }
-    }
-    free(pager);
     free(table);
 }
 
