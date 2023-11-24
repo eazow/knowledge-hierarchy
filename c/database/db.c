fbf358930390b4652b452f4d906ac8955ad7527c
Eazow <eazow@163.com>
2023-11-24 23:20:32 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -192,14 +192,16 @@ void *get_page(Pager *pager, uint32_t page_num)
 // void *row_slot(Table *table, uint32_t row_num)
 void *cursor_value(Cursor *cursor)
 {
-    uint32_t row_num = cursor->row_num;
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    // uint32_t row_num = cursor->row_num;
+    // uint32_t page_num = row_num / ROWS_PER_PAGE;
     // void *page = get_page(table->pager, page_num);
+    uint32_t page_num = cursor->page_num;
     void *page = get_page(cursor->table->pager, page_num);
 
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-    return page + byte_offset;
+    // uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    // uint32_t byte_offset = row_offset * ROW_SIZE;
+    // return page + byte_offset;
+    return leaf_node_value(page, cursor->cell_num);
 }
 
 void cursor_advance(Cursor *cursor)
