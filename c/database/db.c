c87db2e686998221dbecb3f6dc2ffb9ac4a8f042
Eazow <eazow@163.com>
2023-12-11 23:26:11 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -581,6 +581,16 @@ void insert_leaf_node(Cursor *cursor, uint32_t key, Row *row)
     serialize_row(row, leaf_node_value(node, cursor->cell_num));
 }
 
+
+void print_constants() {
+    printf("ROW_SIZE: %d\n", ROW_SIZE);
+    printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+    printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+    printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
 /**
 column   size(bytes) offset
 id       4           0
