f9ea3fe72cfe7d7b0dea3b7b6727238eecbca194
Eazow <eazow@163.com>
2023-12-13 00:01:25 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -366,6 +366,11 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
         db_close(table);
         exit(EXIT_SUCCESS);
     }
+    else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+        printf("Constants:\n");
+        print_constants();
+        return META_COMMAND_SUCCESS;
+    }
     else
     {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
