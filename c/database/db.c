6824118fc65c95752736762a043b5731bb2fe93e
Eazow <eazow@163.com>
2023-12-17 23:33:20 +0800

    Update db

--- c/database/db.c
+++ c/database/db.c
@@ -366,6 +366,11 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
         db_close(table);
         exit(EXIT_SUCCESS);
     }
+    else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+        printf("Tree:\n");
+        print_leaf_node(get_page(table->pager, 0));
+        return META_COMMAND_SUCCESS;
+    }
     else if (strcmp(input_buffer->buffer, ".constants") == 0) {
         printf("Constants:\n");
         print_constants();
@@ -597,12 +602,12 @@ void print_constants() {
 }
 
 void print_leaf_node(void* node) {
-  uint32_t num_cells = *leaf_node_num_cells(node);
-  printf("leaf (size %d)\n", num_cells);
-  for (uint32_t i = 0; i < num_cells; i++) {
-    uint32_t key = *leaf_node_key(node, i);
-    printf("  - %d : %d\n", i, key);
-  }
+    uint32_t num_cells = *leaf_node_num_cells(node);
+    printf("leaf (size %d)\n", num_cells);
+    for (uint32_t i = 0; i < num_cells; i++) {
+        uint32_t key = *leaf_node_key(node, i);
+        printf("  - %d : %d\n", i, key);
+    }
 }
 
 
