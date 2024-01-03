3b5358313dcaa87d98cc4fbb708333f7274cce2b
Eazow <eazow@163.com>
2024-01-03 22:49:58 +0800

    Add send.py

---
+++ python/scapy/send.py
@@ -0,0 +1,11 @@
+from scapy.all import Ether, IP, ICMP, sendp
+
+eth = Ether(src="00:11:22:33:44:55")
+
+ip = IP(src="192.168.1.100", dst="10.67.19.193")
+
+icmp = ICMP()
+
+packet = eth / ip / icmp
+
+sendp(packet)
