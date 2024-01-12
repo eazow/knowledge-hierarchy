6bd3e56b90e064765b4fca91ec385b10f2465851
Eazow <eazow@163.com>
2024-01-13 00:47:20 +0800

    Format

--- c/database/db.c
+++ c/database/db.c
@@ -88,7 +88,6 @@ typedef enum
     NODE_LEAF
 } NodeType;
 
-
 /*
  * Common Node Header Layout
  */
@@ -125,7 +124,6 @@ const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZ
 const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
 const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
 const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
-
 
 // const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 // const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
@@ -368,12 +366,14 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table)
         db_close(table);
         exit(EXIT_SUCCESS);
     }
-    else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+    else if (strcmp(input_buffer->buffer, ".btree") == 0)
+    {
         printf("Tree:\n");
         print_leaf_node(get_page(table->pager, 0));
         return META_COMMAND_SUCCESS;
     }
-    else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+    else if (strcmp(input_buffer->buffer, ".constants") == 0)
+    {
         printf("Constants:\n");
         print_constants();
         return META_COMMAND_SUCCESS;
@@ -437,9 +437,9 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 
 ExecuteResult execute_insert(Statement *statement, Table *table)
 {
-    void* node = get_page(table->pager, table->root_page_num);
+    void *node = get_page(table->pager, table->root_page_num);
     // if (table->num_rows >= TABLE_MAX_ROWS)
-    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
+    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS))
     {
         return EXECUTE_TABLE_FULL;
     }
@@ -451,7 +451,6 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
     // serialize_row(row_to_insert, cursor_value(cursor));
     // table->num_rows += 1;
     insert_leaf_node(cursor, row_to_insert->id, row_to_insert);
-
 
     free(cursor);
 
@@ -593,8 +592,8 @@ void insert_leaf_node(Cursor *cursor, uint32_t key, Row *row)
     serialize_row(row, leaf_node_value(node, cursor->cell_num));
 }
 
-
-void print_constants() {
+void print_constants()
+{
     printf("ROW_SIZE: %d\n", ROW_SIZE);
     printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
     printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
@@ -603,15 +602,16 @@ void print_constants() {
     printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
 }
 
-void print_leaf_node(void* node) {
+void print_leaf_node(void *node)
+{
     uint32_t num_cells = *leaf_node_num_cells(node);
     printf("leaf (size %d)\n", num_cells);
-    for (uint32_t i = 0; i < num_cells; i++) {
+    for (uint32_t i = 0; i < num_cells; i++)
+    {
         uint32_t key = *leaf_node_key(node, i);
         printf("  - %d : %d\n", i, key);
     }
 }
-
 
 /**
 column   size(bytes) offset
